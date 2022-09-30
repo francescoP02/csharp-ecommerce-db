c06e40444a50a94ea6943b7c0d91c925973fc108
francescoP02 <file.partipilo@gmail.com>
2022-09-30 16:34:56 +0200

    Added product adding method only for logged user

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ EcommerceContext db = new EcommerceContext();
 //db.SaveChanges();
 
 string choice;
+string userInput;
 do
 {
     Console.WriteLine("What do you want to do?");
@@ -21,11 +22,13 @@ do
     Console.WriteLine("-Are you register? 'login' for login");
     Console.WriteLine("-For terminate the process insert 'exit'");
     choice = Console.ReadLine();
+    bool logged = false;
     switch(choice)
     {
         case ("register"):
             try
             {
+                Console.Clear();
                 Customer newCustomer = new Customer();
                 Console.WriteLine("Insert name:");
                 string newCustomeName = Console.ReadLine();
@@ -39,16 +42,25 @@ do
 
                 db.Customer.Add(newCustomer);
                 db.SaveChanges();
+
+                logged = true;
+
+                if (logged)
+                {
+                    Main(newCustomer);
+                }
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Invalid values");
+                Console.WriteLine();
             }
             break;
 
         case ("login"):
             try
             {
+                Console.Clear();
                 Console.WriteLine("Insert your email:");
                 string loginMail = Console.ReadLine();
 
@@ -58,13 +70,63 @@ do
                             where s.Email == loginMail
                             select s).First();
                 Console.WriteLine($"Hello {customer.Name}");
+                logged = true;
+
+                if (logged)
+                {
+                    Main(customer);
+                }
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Email not found");
+                Console.WriteLine();
             }
             break;
     }
 
 }while (choice != "exit");
+
+void Main(Customer customer){
+    Console.WriteLine("Inizia");
+    do
+    {
+        Console.WriteLine("What do you want to do?");
+        Console.WriteLine("-If you want to add a product insert 'product'");
+        Console.WriteLine("-For terminate the process insert 'stop'");
+        userInput = Console.ReadLine();
+        switch (userInput)
+        {
+            case ("product"):
+                try
+                {
+                    Console.Clear();
+                    Console.WriteLine("Inserisci un nuovo prodotto: ");
+                    Console.WriteLine("Inserisci il nome del prodotto: ");
+                    string productName = Console.ReadLine();
+
+                    Console.WriteLine("Inserisci la descrizione del prodotto: ");
+                    string productDescription = Console.ReadLine();
+
+                    Console.WriteLine("Inserisci il prezzo del prodotto: ");
+                    double productPrice = double.Parse(Console.ReadLine());
+
+                    using (EcommerceContext context = new EcommerceContext())
+                    {
+                        Product newProduct = new Product { Name = productName, Description = productDescription, Price = productPrice };
+                        context.Add(newProduct);
+                        context.SaveChanges();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Invalid values");
+                    Console.WriteLine();
+                }
+                break;
+        }
+
+    } while (userInput != "stop");
+
+}
 
